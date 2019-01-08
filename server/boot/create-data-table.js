f9c03ed5cafa6ea576d478660521158580aa5049
Machado <a90machado@gmail.com>
2019-01-08 02:00:41 +0000

    problem solved
    problem, on start server delete all users and create new table with the user defauld;
    
    solved, now check if schema exists first, and if not create table and user defauld, if exist continues

--- server/boot/create-data-table.js
+++ server/boot/create-data-table.js
@@ -1,16 +1,21 @@
 module.exports = function(app) {
-  app.dataSources.mysqlDs.automigrate('Account', function(err) {
-    if (err) throw err;
-    app.models.Account.create([{
-      role: 'Admin',
-      createdAt: '2018-12-21T02:16:50.464Z',
-      name: 'andre',
-      username: 'machado',
-      email: 'm@gmail.com',
-      emailVerified: true,
-      password: "abc"
-    }], function(err, Account) {
-      if (err) throw err;
-    });
+  app.dataSources.mysqlDs.isActual('Account', function(err, actual) {
+    if (!actual) {
+      app.dataSources.mysqlDs.autoupdate('Account', function(err) {
+        if (err) throw err;
+        app.models.Account.create( [{
+          role: 'Admin',
+          createdAt: '2018-12-21T02:16:50.464Z',
+          name: 'andre',
+          username: 'machado',
+          email: 'm@gmail.com',
+          emailVerified: true,
+          password: "abc"
+        }], function(err, Account) {
+          if (err) throw err;
+        });
+      });
+    }
   });
+
 };
